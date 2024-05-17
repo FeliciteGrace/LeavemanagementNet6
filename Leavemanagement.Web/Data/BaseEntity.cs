69c27a1024ca83f683eea5c08601ed9ae327d706
Priyanka Rana <priyanka136rana@gmail.com>
2024-05-17 11:54:09 +0530

    added db tables and migration updated database

---
+++ Leavemanagement.Web/Data/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Leavemanagement.Web.Data
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
---
+++ Leavemanagement.Web/Data/Employee.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Leavemanagement.Web.Data
+{
+    public class Employee : IdentityUser
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string? Firstname { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string? Lastname { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string? TaxId { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime DateOfBirth { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime DateJoined { get; set; }
+
+    }
+}
